d18f70a378f3f18096e6d3ea2d7ca79a6285f7c0
ChocoFloco <149216961+ChocoFloco@users.noreply.github.com>
2023-11-15 16:35:33 -0600

    Update config.dist.js

--- src/config/config.dist.js
+++ src/config/config.dist.js
@@ -1,11 +1,11 @@
 /* Get these settings from your firebase account */
 export const DB_CONFIG = {
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: ''
+    apiKey: 'AIzaSyC7mybMry1LzLSlpsmXMlN_9LbcPogoW2g',
+    authDomain: 'inverseprivate-304e0.firebaseapp.com',
+    databaseURL: 'https://inverseprivate-304e0-default-rtdb.firebaseio.com',
+    projectId: 'inverseprivate-304e0',
+    storageBucket: 'inverseprivate-304e0.appspot.com',
+    messagingSenderId: '246587992179'
 };
 
 export const SERVER = {
